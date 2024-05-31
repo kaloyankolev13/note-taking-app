a2c1b9a9e35c41c9efbd3955e7d783444c155a15
kaloyankolev13 <63497199+kaloyankolev13@users.noreply.github.com>
2024-05-31 23:28:53 +0200

    make it better

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,24 @@ import { TaskModule } from './task/task.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TaskModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    TaskModule,
     UsersModule,
     AuthModule,
     ProjectModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/note-app'),
   ],
 })
 export class AppModule {}
